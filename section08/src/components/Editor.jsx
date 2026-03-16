8f537dda806a30ff6aad904f6af8d7e68d8dc9ae
kminjoo9093 <kminjoo9093@naver.com>
2026-03-17 01:21:48 +0900

    section08 - TodoList : Create

---
+++ section08/src/components/Editor.jsx
@@ -0,0 +1,41 @@
+import "./Editor.css";
+import { useState, useRef } from "react";
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus(); //비었을 경우 인풋창 포커스
+      return;
+    }
+    onCreate(content);
+    setContent(""); //추가 후 인풋창 초기화
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        value={content}
+        ref={contentRef}
+        onChange={onChangeContent}
+        onKeyDown={onKeyDown}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
