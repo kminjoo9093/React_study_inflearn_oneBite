df54269d853fa668678d0f1f9fa238a5dcd2cccd
kminjoo9093 <kminjoo9093@naver.com>
2026-03-17 21:12:34 +0900

    useReducer로 todolist 최적화

---
+++ section09/src/components/List.jsx
@@ -0,0 +1,43 @@
+import "./List.css";
+import { useState } from "react";
+import TodoItem from "./TodoItem";
+
+const List = ({ todos, onUpdate , onDelete}) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+
+    return todos.filter((todo) => {
+      return todo.content.toLowerCase().includes(search.toLowerCase());
+    });
+  };
+
+  const filteredTodos = getFilteredData();
+
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} onDelete={onDelete}/>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
+
+//검색어가 변경되면 리스트 컴포넌트가 리렌더링되어야 함 -> 검색어를 state로 보관
