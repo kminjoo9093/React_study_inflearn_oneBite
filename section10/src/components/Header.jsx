e591e51008b94e95025f53010acca584267e502d
kminjoo9093 <kminjoo9093@naver.com>
2026-03-19 00:46:35 +0900

    section10 : React.memo로 컴포넌트 리렌더링 제어

--- section10/src/components/Header.jsx
+++ section10/src/components/Header.jsx
@@ -1,10 +1,13 @@
 import "./Header.css";
+import { memo } from "react";
 
-const Header = ()=>{
-  return <div className="Header">
-    <h3>오늘은 📅</h3>
-    <h1>{new Date().toDateString()}</h1>
-  </div>
-}
+const Header = () => {
+  return (
+    <div className="Header">
+      <h3>오늘은 📅</h3>
+      <h1>{new Date().toDateString()}</h1>
+    </div>
+  );
+};
 
-export default Header;
+export default memo(Header);
--- section10/src/components/TodoItem.jsx
+++ section10/src/components/TodoItem.jsx
@@ -1,21 +1,35 @@
 import "./TodoItem.css";
+import { memo } from "react";
 
-const TodoItem = ({id, isDone, content, date, onUpdate, onDelete}) => {
-
-  const onChangeCheckBox = ()=>{
+const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+  const onChangeCheckBox = () => {
     onUpdate(id);
   };
 
-  const onClickDeleteButton = ()=>{
+  const onClickDeleteButton = () => {
     onDelete(id);
-  }
+  };
 
-  return <div className="TodoItem">
-    <input onChange={onChangeCheckBox} checked={isDone} type="checkbox"/>
-    <div className="content">{content}</div>
-    <div className="date">{new Date(date).toLocaleDateString()}</div>
-    <button onClick={onClickDeleteButton}>삭제</button>
-  </div>
-}
+  return (
+    <div className="TodoItem">
+      <input onChange={onChangeCheckBox} checked={isDone} type="checkbox" />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onClickDeleteButton}>삭제</button>
+    </div>
+  );
+};
 
-export default TodoItem;
+//고차 컴포넌트
+export default memo(TodoItem, (prevProps, nextProps) => {
+  // 반환값에 따라, props가 바뀌었는지 안바뀌었는지 판단
+  // True => props가 바뀌지 않음 -> 리렌더링 x
+  // false => props가 바뀜 -> 리렌더링 o
+
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.isDone !== nextProps.isDone) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  if (prevProps.date !== nextProps.date) return false;
+
+  return true;
+});
