3622f00386c6b2cb28aadf273e0ed886ed3e40e1
kminjoo9093 <kminjoo9093@naver.com>
2026-03-18 23:53:50 +0900

    section10 : useMemo

---
+++ section10/src/components/List.jsx
@@ -0,0 +1,85 @@
+import "./List.css";
+import { useState, useMemo } from "react";
+import TodoItem from "./TodoItem";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+
+    return todos.filter((todo) => {
+      return todo.content.toLowerCase().includes(search.toLowerCase());
+    });
+  };
+
+  const filteredTodos = getFilteredData();
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("getAnalizedData 호출");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((item) => item.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+  //의존성 배열 deps
+
+
+  // const getAnalizedData = () => {
+  //   console.log("getAnalizedData 호출");
+  //   const totalCount = todos.length;
+  //   const doneCount = todos.filter((item) => item.isDone).length;
+  //   const notDoneCount = totalCount - doneCount;
+
+  //   return {
+  //     totalCount,
+  //     doneCount,
+  //     notDoneCount,
+  //   };
+  // };
+
+  // const { totalCount, doneCount, notDoneCount } = getAnalizedData();
+
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <div>
+        <div>total : {totalCount}</div>
+        <div>done : {doneCount}</div>
+        <div>notDone : {notDoneCount}</div>
+      </div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
+
+//검색어가 변경되면 리스트 컴포넌트가 리렌더링되어야 함 -> 검색어를 state로 보관
